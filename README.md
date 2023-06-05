37aa43584effac2a378473df3add414e7f56a934
asfuz <ubuntusamandar@gmail.com>
2023-06-05 12:38:15 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# bazzar - a fresh grocery store
+
+"I will not be impressed with technology until I can download food from the internet" - Carlos Banks
+
+So here you go, now you have chance to download food from the internet :)
+
+Have a fun ;)
+
+OnBoarding Page             |  Login Page
+:--------------------------:|:-------------------------:
+![image](https://user-images.githubusercontent.com/70211629/209198858-f9fd180d-dc12-42f7-a2a5-db7b368aa284.png)    |  ![image](https://user-images.githubusercontent.com/70211629/209198874-3f5be95a-cc32-4004-9419-2b6b12078fb8.png)
+
+Home Page             |  Product Page
+:--------------------:|:-------------------------:
+![image](https://user-images.githubusercontent.com/70211629/209198938-77c11b2e-637d-4e08-bb89-acd95367ce1b.png)    |  ![image](https://user-images.githubusercontent.com/70211629/209198952-a8a4efdc-2578-4796-8527-19b6e04f3256.png)
+
+Cart Page             |  Order Page
+:--------------------:|:-------------------------:
+![image](https://user-images.githubusercontent.com/70211629/209200236-cddb0b44-065e-40a8-97e1-9309e06f0b8c.png)    |  ![image](https://user-images.githubusercontent.com/70211629/209200253-a4369aaa-8cc2-4e30-8786-10da4796bbb6.png)
+
+Order Failed             |  Order Accepted
+:--------------------:|:-------------------------:
+![image](https://user-images.githubusercontent.com/70211629/209200633-ddb31929-3f50-4c10-878b-9ea4b8e58e9b.png)    |  ![image](https://user-images.githubusercontent.com/70211629/209200679-966e6745-1d1d-405e-90d9-be0543fd986c.png)
---
+++ lib/widgets/bars/navigationBar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:bazzar/pages/productPages/cartPage.dart';
+import 'package:bazzar/pages/productPages/explorerPage.dart';
+import 'package:bazzar/pages/productPages/favoritePage.dart';
+import 'package:bazzar/pages/productPages/profilePage.dart';
+
+import '../../pages/productPages/homePage.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentNavIndex = 0;
+  final screens = [
+    const HomePage(),
+    const ExplorerPage(),
+    const CartPage(),
+    const FavoritePage(),
+    const ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16,
+      borderRadius: BorderRadius.circular(15),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: SizedBox(
+        height: 70,
+        child: BottomNavigationBar(
+          elevation: 0,
+          currentIndex: _currentNavIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/home.png'),
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/explorer.png'),
+                ),
+                label: 'Explorer'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/cart.png'),
+                ),
+                label: 'Cart'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/favorite.png'),
+                ),
+                label: 'Favorite'),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/profile.png'),
+                ),
+                label: 'Profile'),
+          ],
+        ),
+      ),
+      
+    );
+    
+  }
+  void _onItemTapped(int index) {
+      setState(() {
+        _currentNavIndex = index;
+      });
+    }
+}
